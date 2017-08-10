325ab4925f32975406bede5bd1832b977d9b0da9
Abdelkrim BENHAMMOU <abbenham@e3r12p2.42.fr>
2017-08-10 15:45:09 +0200

    a la norme

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -7,39 +7,32 @@ int		ft_putchar(char c)
 	write(1, &c, 1);
 	return (0);
 }
+
 int		ft_double_occ(char *s1, char *s2, int conf)
 {
 	int		i;
 
 	i = 0;
-	//printf("\nINIT:  conf = %d, i = %d, s1: %s, s2: %s\n",conf, i, &s1[conf], s2);
-
 	while (s1[i + conf])
 	{
-		//printf("conf = %d, i = %d, s1: %c, s2: %c,\n",conf, i,s1[i + conf],s2[i]);
 		if (s1[i + conf] == s2[i])
-		{
 			return (1);
-
-		}
 		i++;
 	}
 	if (s1[conf] != '\0')
-		return(ft_double_occ(s1, s2, conf + 1));
+		return (ft_double_occ(s1, s2, conf + 1));
 	return (0);
 }
-
-
 
 int		ft_lbase(char *base)
 {
 	int i;
-	
+
 	i = 0;
 	while (base[i])
 	{
-		if (ft_double_occ(base,base,1) == 1)
-			return (0);	
+		if (ft_double_occ(base, base, 1) == 1)
+			return (0);
 		if (base[i] == '+' || base[i] == '-')
 			return (0);
 		i++;
